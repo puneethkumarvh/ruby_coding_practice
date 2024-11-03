7155d4b5191771b7dece4d1ed7f00ed751f5f0ca
Puneeth kumar vh <69886724+puneethkumarvh@users.noreply.github.com>
2024-11-03 11:38:27 +0530

    Update easy_questions.rb

--- easy_questions.rb
+++ easy_questions.rb
@@ -16,3 +16,64 @@ given_string.each_char do |x|
 end 
 p "vowels : #{vowels_count}"
 p "consonants : #{consonants_count}"
+
+
+//Question : Reverse a string without using built in methods
+
+given_string = "amma"
+reversed=""
+for  i in (given_string.length-1).downto(0)
+    reversed+=given_string[i]
+end
+p reversed
+
+
+//Question : Split the words in a sentence
+
+given_string = "amma is my god!."
+split_string=given_string.split(" ")
+p split_string
+
+//Question : Find the prime numbers in an array
+num =25
+if num<=1 
+    p false
+end 
+
+(2..Math.sqrt(num)).each do |i|
+    if num % i ==0 
+        p false 
+    end 
+end 
+p true 
+
+//Question : Find the highest number in an array
+num = [1,5,2,10,29,32,4,16,01]
+max= num[0]
+num.each do |x|
+ if x > max
+   max = x
+ end 
+end 
+p max
+
+//Question : Find the 3rd lowest number in an array
+num = [1,2,3,4,5,6,7]
+first_lowest = second_lowest = third_lowest = Float:: INFINITY
+
+num.each do |x|
+ if x < first_lowest 
+   third_lowest = second_lowest
+   second_lowest = first_lowest 
+   first_lowest = x
+ elsif x < second_lowest
+   third_lowest = second_lowest
+   second_lowest = x
+ elsif x < third_lowest 
+   third_lowest = x
+ end 
+end 
+p third_lowest 
+
+
+
